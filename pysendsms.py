c7536028ecdbe4b04c45d961d09148896429b0d8
Jolth <jolthgs@gmail.com>
2016-03-08 13:52:27 -0500

    test arg

--- pysendsms.py
+++ pysendsms.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import argparse
+import threading
+from string import Template
+import queue
+import gammu
+
 
 # Configure Arguments:
 aparser = argparse.ArgumentParser(prog='pysendsms', description='send SMS\
@@ -8,8 +13,34 @@ aparser = argparse.ArgumentParser(prog='pysendsms', description='send SMS\
 group = aparser.add_mutually_exclusive_group()
 group.add_argument('-f', '--file',  help='file CSV to reading')
 group.add_argument('-m', '--message', help='message to send')
-
-aparser.add_argument(
-        "-t", "--template", help="template for the message")
+group.add_argument('-t', '--template', help='template for the message. i.e:\
+        \'Mr. [$name|${name}] like this\'')
+aparser.add_argument('-n', '--number', help='number to sms send')
 args = aparser.parse_args()
+
+
+class SendSMS(threading.Thread):
+    def __init__(self):
+        pass
+
+
+
+if __name__ == '__main__': 
+    fifo = queue.Queue()
+
+
+    print(args)
+
+    #print(args.template)
+    t = args.template
+    if t:
+        t = Template(t)
+    else:
+        print(t or '')
+
+    #t = Template(args.template) or None
+    #if t:
+    #    print(t.substitute(name='jorge'))
+    #else:
+    #    print(t)
 
